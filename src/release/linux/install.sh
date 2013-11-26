0fa0b3916941d02f8f6affd4c04bd1bed5bf0486
Jakub Szwacz <jakub@szwacz.com>
2013-11-26 22:02:29 +0100

    linux installation works on all major distros

--- src/release/linux/install.sh
+++ src/release/linux/install.sh
@@ -14,9 +14,16 @@ cp -r -f ./Sputnik /opt
 cp -f ./Sputnik.desktop /usr/share/applications
 
 # solution for lacking libudev.so.0
-set -e
-udiskbin=`which udisks`
-udevso=`ldd $udiskbin | grep libudev.so | awk '{print $3;}'`
+udevDependent=`which udisks 2> /dev/null` # Ubuntu, Mint
+if [ -z "$udevDependent" ]
+then
+    udevDependent=`which systemd 2> /dev/null` # Fedora, SUSE
+fi
+if [ -z "$udevDependent" ]
+then
+    udevDependent=`which findmnt` # Arch
+fi
+udevso=`ldd $udevDependent | grep libudev.so | awk '{print $3;}'`
 if [ -e "$udevso" ]; then
     ln -sf "$udevso" /opt/Sputnik/libudev.so.0
 fi
