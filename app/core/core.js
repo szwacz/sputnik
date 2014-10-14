ec0a2b708655a45d303812b860e9671b8b7d1ed6
Jakub Szwacz <jakub@szwacz.com>
2014-10-14 21:06:14 +0200

    Imports fixed

--- app/core/core.js
+++ app/core/core.js
@@ -5,6 +5,8 @@ import feedsService from './services/feeds';
 import articlesService from './services/articles';
 import downloadService from './services/downloader';
 import faviconsService from './services/favicons';
+import feedParser from './services/feed_parser';
+import scout from './services/feed_scout';
 
 var definition = {
     name: 'core'
@@ -20,6 +22,8 @@ angular.module(definition.name, [])
 .service('downloadService', downloadService)
 .service('faviconsService', faviconsService)
 .service('feedsService', feedsService)
+.service('feedParser', feedParser)
+.service('scout', scout)
 .run(function (schedule) {
     schedule.start();
 });
